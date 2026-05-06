6def07cf3b8c0aa6a481acb3ac9db3b3881888b9
Lara Li <lrli@usc.edu>
2026-05-05 20:17:40 -0700

    Update tests.py

--- src/tests.py
+++ src/tests.py
@@ -2,7 +2,7 @@
 from load import get_json_data, get_csv_data
 from process import process_cdc_data, process_crime_data, process_census_data, merge_datasets
 from config import CDC_URL, FBI_URL, CENSUS_URL, RESULTS_DIR, DATA_DIR
-from analyze import plot_bar, plot_scatter, plot_heatmap, run_clustering
+from analyze import plot_bar, plot_scatter, plot_heatmap, run_clustering, run_clustering_confound
 
 if __name__ == "__main__":
     
@@ -50,4 +50,18 @@ if __name__ == "__main__":
     else:
         print(f"Merge returned {len(merged)} rows")
 
+    # test analysis 
+    plot_bar(merged, 'mental_health_pct', 'Poor Mental Health (%) – Past 30 Days', 'Poor Mental Health % by State', RESULTS_DIR)
+    plot_bar(merged, 'rate_per_100k', 'Violent Crime Rate (per 100k ppl)','Violent Crime Rate by State', RESULTS_DIR)
+    plot_bar(merged, 'poverty_rate', 'Poverty Rate', 'Poverty Rate by State', RESULTS_DIR)
+    plot_bar(merged, 'unemployment_rate', 'Unemployment Rate','Unemployment Rate by State', RESULTS_DIR)
+    plot_bar(merged, 'no_insurance_pct', '% with No Insurance','% No Insurance Rate by State', RESULTS_DIR)
+
+    plot_scatter(merged, 'mental_health_pct','rate_per_100k', 'Poor Mental Health (%) – Past 30 Days', 'Violent Crime Rate (per 100k ppl)', 'Mental Health x Crime Scatter Plot',RESULTS_DIR)
+
+    plot_heatmap(merged,RESULTS_DIR)
+
+    run_clustering(merged,RESULTS_DIR)
+        
+    run_clustering_confound(merged,RESULTS_DIR)
 
